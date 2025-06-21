653031d500ef7cfa9c9a7c6461499253febbb9b9
Thang Truong <thangtruong@Thangs-MacBook-Pro.local>
2025-06-21 18:36:16 -0500

    change wording

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function scrollFunction() {
 
 
 
-const roles = ["a Software Engineer.", "an AI fanatic.", "an AMC A-list member."];
+const roles = ["a Software Engineer.", "an AI fanatic.", "an AMC VIP member."];
 const color = ['red', 'blue', 'black']
 const typewriterSpan = document.getElementById("typewriter");
 
